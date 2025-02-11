b1725a1d7fa01e3bbb56794a145efb8c78b6e905
Eric Shen <500001049@my.fiveislands.uwi.edu>
2025-02-10 21:04:50 -0400

    .

---
+++ MyLaravel/resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Eric Shen</h2>
+    <p>My name is Eric Shen. I am a second year student at the UWI FIC, currently studying under
+        the School of Science, Computing and Artificial Intelligence. I am currently pursuing a BSc
+        in Computer Science and I am also a full-time student. I am currently undertaking the
+        COMP3385 course, which is a course in Web Development.
+    </p>
+@endsection
---
+++ MyLaravel/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+use App\Http\Controllers\AboutController; 
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', [AboutController::class, 'show']); {
+    return "My About Page";
+} ;
